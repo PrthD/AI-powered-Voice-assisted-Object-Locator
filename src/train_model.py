3c4e2b3260d6819ecb5c14d41ab84e492bff9fbc
sahoo <csahoo@ualberta.ca>
2025-03-10 18:08:58 +0000

    More changes to triner.

--- src/train_model.py
+++ src/train_model.py
@@ -2,10 +2,10 @@ import torch
 from ultralytics import YOLO
 
 #Create the model from the .pt file
-model = YOLO("../models/yolov8s.pt")
+model = YOLO("../models/yolo/yolov8s.pt")
 
 #Train the model on the object 365 data
-results = model.train(data="../models/Objects365.yaml", epochs=3)
+results = model.train(data="../models/yolo/Objects365.yaml", epochs=3)
 
 #Save the model
-torch.save(model.state_dict(), "../models/yolov8s.pt")
+torch.save(model.state_dict(), "../models/yolo/yolov8s.pt")

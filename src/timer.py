867afe3b7a4ea412aec58784e96914728d808766
Chinmoy03 <115158571+Chinmoy03@users.noreply.github.com>
2025-03-31 15:37:44 -0600

    Update timer.py
    Added functionality to check log completeness and save the log to time_statistics.xlsx

--- src/timer.py
+++ src/timer.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-import pandas
+import pandas as pd
 
 class Timer:
   def __init__(self) -> None:
@@ -58,4 +58,39 @@ class Timer:
                            "end": None}
     self.time_dict["ts"] = {"start": None,
                             "end": None}
+  
+  def save_log(self)-> bool:
+    if self.check_log_completeness() == False:
+      return False
+    data = pd.read_excel("time_statistics.xlsx")
+    new_field = pd.DataFrame(columns = ['Session', 'Speech-to-Text', 'Object Detection', 'Text-to-Speech',
+ 'Total Time'])
+    new_field['Session'] = self.time_dict["time"]
+    new_field['Speech-to-Text'] = self.time_dict["st"]["end"] - self.time_dict["st"]["start"]
+    new_field['Object Detection'] = self.time_dict["d"]["end"] - self.time_dict["d"]["start"]
+    new_field['Text-to-Speech'] = self.time_dict["ts"]["end"] - self.time_dict["ts"]["start"]
+    new_field['Total Time'] = new_field['Speech-to-Text'] + new_field['Object Detection'] + new_field['Text-to-Speech']
+    data = pd.concat([data, new_field], ignore_index = True)
+    data.to_excel("time_statistics.xlsx", index = False)
+    return True
 
+  def check_log_completeness(self):
+    if self.time_dict["st"]["start"] == None:
+      return False
+    elif self.time_dict["st"]["end"] == None:
+      return False
+    elif self.time_dict["d"]["start"] == None:
+      return False
+    elif self.time_dict["d"]["end"] == None:
+      return False
+    elif self.time_dict["ts"]["start"] == None:
+      return False
+    elif self.time_dict["ts"]["end"] == None:
+      return False
+    else:
+      return True
+    
+
+# if __name__ == '__main__':
+#     timer = Timer()
+#     timer.save_log()
